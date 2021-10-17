36378ed5f42d4a490219a8d6ad273c7d3bd52225
pot-noodle-python <faizanshah123123123@gmail.com>
2021-10-17 09:57:21 +0100

    half complete submit page

--- create_posts.py
+++ create_posts.py
@@ -1,23 +1,19 @@
 import requests
-from essential_generators import DocumentGenerator
 import threading
 
-gen = DocumentGenerator()
 
 
 def create_post():
-    for i in range(10):
+    for i in range(100):
         res = requests.post(
             "http://localhost:5000/api/v1/posts/",
             json={
-                "title": gen.word(),
-                "desc": gen.word(),
-                "tags": [gen.word(), gen.word(), gen.word()],
+                "title": "Test",
+                "desc": "This was made from the API",
+                "tags": ["API", "cool"],
             },
         )
         print(res.status_code)
 
 
-for i in range(10):
-    t = threading.Thread(target=create_post)
-    t.start()
+create_post()
--- website/static/js/index.js
+++ website/static/js/index.js
@@ -24,6 +24,7 @@ function add_tag() {
         input.name = "tag" + inputCount;
         strcount = String(parseInt(inputCount) + 1)
         input.placeholder = "Enter tag " + strcount;
+        input.style.cssText = "display: inline-flex;box-shadow: inset 0 .0625em .125em rgba(10,10,10,.05);width:  100%;height: 2.5rem;padding: calc(.5em - 1px) calc(.75em - 1px);align-items: center;justify-content: flex-start;border: 1px solid #dfdfdf;color: #363636;border-radius: 4px;font-family: var(--font-family);"
         container.appendChild(input);
         inputCount++;
     }
